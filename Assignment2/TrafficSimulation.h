615e81175817687a64502ef3d46544e81a930f61
‘Goo’ <yoedgrizim@gmail.com>
2014-11-24 17:13:14 +0200

    by yoed Roads.h , TrafficSimulatin.h, Commands.h, Junctions.h

---
+++ Assignment2/TrafficSimulation.h
@@ -0,0 +1,25 @@
+#ifndef _TRAFFICSIMULATION_H_
+#define _TRAFFICSIMULATION_H_
+
+// THIS IS THE DECLARATION FILE OF THE CLASS TrafficSimulation (TrafficSimulation.h) 
+  class TrafficSimulation
+  {
+  private:
+	 Vactor Event_;
+	 Vactor Commands_;
+     Vactor Roads_;  
+	 Vactor Cars_;
+	 int SimulationTime;
+  public:
+     TrafficSimulation();
+     TrafficSimulation(????);
+     TrafficSimulation(const TrafficSimulation& copyTrafficSimulation);
+     virtual ~TrafficSimulation();
+     TrafficSimulation& operator=(const TrafficSimulation &TrafficSimulationRightArg);
+	 
+	 initTrafficSimulation();
+	 simulationManagement();
+	 
+  };   
+  
+  #endif //_TRAFFICSIMULATION_H_
